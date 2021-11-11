de1aa19e32f205790e7b74921783ef9ca950d344
Yasin Hirani <yasin.hirani@1rivet.local>
2021-11-11 20:21:50 +0530

    Fix: fetch url

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -10,6 +10,6 @@ export class NewsService {
   constructor(private http: HttpClient) { }
 
   getNewsService(catagory: any){
-    return this.http.get(`http://newsapi.org/v2/top-headlines?country=in&category=${catagory}&apiKey=6dd98cb4dec143c59c6b2ea329004c53`);
+    return this.http.get(`https://newsapi.org/v2/top-headlines?country=in&category=${catagory}&apiKey=6dd98cb4dec143c59c6b2ea329004c53`);
   }
 }
